901f6ab72ed3886e272b2303009f0318ea131052
Nageswara Rao D <nageswararao.dolla@gmail.com>
2025-07-12 16:52:03 +0530

    test

--- main.tf
+++ main.tf
@@ -1,6 +1,7 @@
 resource "aws_instance" "Nageswar" {
   ami           = "ami-09c813fb71547fc4f"
   instance_type = "t2.micro"
+  vpc_security_group_ids = [ "sg-04e1d05a8c19f60d1" ]
 
   tags = {
     Name = "Nageswar"
@@ -10,6 +11,7 @@ resource "aws_instance" "Nageswar" {
 resource "aws_instance" "Nageswar1" {
   ami           = "ami-09c813fb71547fc4f"
   instance_type = "t2.micro"
+  vpc_security_group_ids = [ "sg-04e1d05a8c19f60d1" ]
 
   tags = {
     Name = "Nageswar1"
@@ -19,6 +21,7 @@ resource "aws_instance" "Nageswar1" {
 resource "aws_instance" "Nageswar2" {
   ami           = "ami-09c813fb71547fc4f"
   instance_type = "t2.micro"
+  vpc_security_group_ids = [ "sg-04e1d05a8c19f60d1" ]
 
   tags = {
     Name = "Nageswar2"
